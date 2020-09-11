72a4bb0855beea8cc6c58b3eb7bfc9a059707439
paulo-petrachini <petrachini@uol.com.br>
2020-09-11 17:51:09 -0300

    Exercicio 46 atualizado

--- src/Empregado.java
+++ src/Empregado.java
@@ -5,7 +5,26 @@ public class Empregado {
 	double salario;
 	
 	void imprimir() {
-		System.out.printf("Nome: "+nome+" Cargo: "+cargo+" Salário: %.2f\n",salario);
+		System.out.println(" ***** Dados do Funcionário *****");
+		System.out.println(" Nome:    "+nome);
+		System.out.println(" Cargo:   "+cargo);
+		System.out.printf(" Salário: %.2f\n",salario);
 	}
-
+	
+	void aumentarSalario() {
+		
+	}
+	double calcularImposto() {
+		double imposto;
+		if (salario < 2000.0){
+			imposto = 0.0;
+		}
+		else if (salario <= 4500.0) {
+			imposto = salario*0.1;
+		}
+		else {
+			imposto = 1200.0;
+		}
+		return imposto;
+	}
 }
--- src/EmpregadoTeste.java
+++ src/EmpregadoTeste.java
@@ -1,12 +1,32 @@
 
 public class EmpregadoTeste {
 	public static void main(String args[]) {
-		Empregado e1;
+		Empregado e1, e2, e3;
 		e1 = new Empregado();
+		e2 = new Empregado();
+		e3 = new Empregado();
 		
+				
 		e1.nome = "Paulo R. Petrachini";
 		e1.cargo = "Analista de Sistemas";
-		e1.salario = 1000.0;
+		e1.salario = 1800.0;
 		e1.imprimir();
+		System.out.printf(" Imposto do E1: %.2f\n",e1.calcularImposto());
+		System.out.println();
+						
+		e2.nome = "Mara C. da S. Cardoso Petrachini";
+		e2.cargo = "Psicóloga";
+		e2.salario = 5000.0;
+		e2.imprimir();
+		System.out.printf(" Imposto do E2: %.2f\n",e2.calcularImposto());
+		System.out.println();
+		
+		
+		e3.nome = "Pedro Cardoso Petrachini";
+		e3.cargo = "Jornalista";
+		e3.salario = 4200.0;
+		e3.imprimir();
+		System.out.printf(" Imposto do E3: %.2f\n",e3.calcularImposto());
+		System.out.println();
 	}
 }
